9c6f120fffc5f92d677e146362352f21dcf8f4bf
bigya <bigyabajracharya@gmail.com>
2020-01-02 23:27:22 +0545

    2.3 done

--- part2/course-contents/src/components/Total.js
+++ part2/course-contents/src/components/Total.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Total = ({ part1, part2, part3 }) => (
-  <p>
-    Number of exercises {part1.exercises + part2.exercises + part3.exercises}
-  </p>
+  <strong><p>
+    total of {part1.exercises + part2.exercises + part3.exercises} exercises
+  </p></strong>
 );
 
 export default Total;
