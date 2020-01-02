42f9fe27d19e70305746d8fee3e91d7ed4e4df5f
bigya <bigyabajracharya@gmail.com>
2020-01-03 00:18:56 +0545

    2.4 done + refactor

--- part2/course-contents/src/components/Content.js
+++ part2/course-contents/src/components/Content.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Part from "./Part";
 
-const Content = ({ parts }) => parts.map(part => <Part part={part} />);
+const Content = ({ parts }) => parts.map(part => <Part part={part} key={part.id} />);
 
 export default Content;
