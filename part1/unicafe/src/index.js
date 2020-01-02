ac6fc9f1f765502cd868872965536881315646ae
bigya <bigya.bajracharya@citytech.global>
2020-01-02 17:18:50 +0545

    1.9 done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -9,36 +9,61 @@ const Button = ({ text, handleClick }) => (
 
 const Statistic = ({ text, number, type }) => (
   <p>
-    {text}: {number} {type==="percentage"? '%': ''}
+    {text}: {number} {type === "percentage" ? "%" : ""}
   </p>
 );
 
-const Statistics = ({good, bad, neutral, average, total, positive}) => 
-<>
-    <Statistic text="good" number={good} />
-    <Statistic text="neutral" number={neutral} />
-    <Statistic text="bad" number={bad} />
-    <Statistic text="all" number={total} />
-    <Statistic text="average" number={average} />
-    <Statistic text="positive" number={positive || 0} type="percentage" />
-</>;
+const Statistics = ({
+  good,
+  bad,
+  neutral,
+  average,
+  total,
+  positive,
+  feedback
+}) =>
+  feedback ? (
+    <>
+      <Statistic text="good" number={good} />
+      <Statistic text="neutral" number={neutral} />
+      <Statistic text="bad" number={bad} />
+      <Statistic text="all" number={total} />
+      <Statistic text="average" number={average} />
+      <Statistic text="positive" number={positive || 0} type="percentage" />
+    </>
+  ) : (
+    "No feedback given"
+  );
 
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [feedback, setFeedback] = useState(false);
   const [goodFlag, neutralFlag, badFlag] = [1, 0, -1];
 
-  const total = good+neutral+bad;
+  const total = good + neutral + bad;
 
-  const average = (good*goodFlag+neutral*neutralFlag+bad*badFlag)/total || 0;
+  const average =
+    (good * goodFlag + neutral * neutralFlag + bad * badFlag) / total || 0;
 
-  const positive = (good/total)*100; 
+  const positive = (good / total) * 100;
 
-  const voteGood = () => setGood(good + 1);
-  const voteNeutral = () => setNeutral(neutral + 1);
-  const voteBad = () => setBad(bad + 1);
+  const voteGood = () => {
+    setGood(good + 1);
+    setFeedback(true);
+  };
+
+  const voteNeutral = () => {
+    setNeutral(neutral + 1);
+    setFeedback(true);
+  };
+
+  const voteBad = () => {
+    setBad(bad + 1);
+    setFeedback(true);
+  };
 
   return (
     <div>
@@ -47,14 +72,16 @@ const App = () => {
       <Button text="neutral" handleClick={voteNeutral} />
       <Button text="bad" handleClick={voteBad} />
       <Header text="statistics" />
-      <Statistics 
-      good={good} 
-      bad={bad}
-      neutral={neutral} 
-      total={total}
-      average={average}
-      positive={positive} />
-</div>
+      <Statistics
+        good={good}
+        bad={bad}
+        neutral={neutral}
+        total={total}
+        average={average}
+        positive={positive}
+        feedback={feedback}
+      />
+    </div>
   );
 };
 
