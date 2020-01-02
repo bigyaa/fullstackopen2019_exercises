95e4148d51018d2c96ff6cd4699b87de63823f59
bigya <bigya.bajracharya@citytech.global>
2020-01-02 15:39:42 +0545

    exercise 1.6 done

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const Header = ({ text }) => <h1>{text}</h1>;
+
+const Button = ({ text, handleClick }) => (
+  <button onClick={handleClick}> {text} </button>
+);
+
+const Statistic = ({text, number}) =>
+<p>{text}: {number} </p>
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const voteGood = () => setGood(good + 1);
+  const voteNeutral = () => setNeutral(neutral + 1);
+  const voteBad = () => setBad(bad + 1);
+
+  return (
+    <div>
+      <Header text="give feedback" />
+      <Button text="good" handleClick={voteGood} />
+      <Button text="neutral" handleClick={voteNeutral} />
+      <Button text="bad" handleClick={voteBad} />
+      <Header text="statistics" />
+      <Statistic text="good" number={good} />
+        <Statistic text="neutral" number={neutral}/>
+        <Statistic text="bad" number={bad}/>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
