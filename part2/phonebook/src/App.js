172140fad845709c0a6736c5dbe0c7d329460ac0
bigya <bigya.bajracharya@citytech.global>
2020-01-03 15:52:01 +0545

    2.9 filter by name

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,29 +2,38 @@ import React, { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "111-222-111" }
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" }
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [nameFilter, setNameFilter] = useState("");
 
   const handleNameChange = event => setNewName(event.target.value);
   const handleNumberChange = event => setNewNumber(event.target.value);
+  const handleNameFilterChange = event => setNameFilter(event.target.value);
 
   const handleSubmit = event => {
     event.preventDefault();
-    if(persons.find(person =>
-      person.name === newName)){
-       alert(`${newName} already exists in the phonebook`)
-      } else {
-        setPersons(persons.concat({ name: newName, number: newNumber }))
-        setNewName("");
-        setNewNumber("");
-      }
+    if (persons.find(person => person.name === newName)) {
+      alert(`${newName} already exists in the phonebook`);
+    } else {
+      setPersons(persons.concat({ name: newName, number: newNumber }));
+      setNewName("");
+      setNewNumber("");
+    }
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter Name:{" "}
+        <input value={nameFilter} onChange={handleNameFilterChange} />
+      </div>
+      <h2>Add a New Person</h2>
       <form onSubmit={handleSubmit}>
         <div>
           Name: <input value={newName} onChange={handleNameChange} />
@@ -40,11 +49,27 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => (
-          <li key={person.name}>
-            {person.name} {person.number}
-          </li>
-        ))}
+        {nameFilter
+          ? persons.map(
+              person =>
+                console.log(
+                  "filter",
+                  person,
+                  person.name.includes(nameFilter)
+                ) ||
+                (person.name
+                  .toLowerCase()
+                  .includes(nameFilter.toLowerCase()) && (
+                  <li key={person.name}>
+                    {person.name} {person.number}
+                  </li>
+                ))
+            )
+          : persons.map(person => (
+              <li key={person.name}>
+                {person.name} {person.number}
+              </li>
+            ))}
       </ul>
     </div>
   );
