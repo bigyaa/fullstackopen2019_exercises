209d05a9784312f7da86a47a4631ac534e7701e0
bigya <bigya.bajracharya@citytech.global>
2020-01-03 15:54:54 +0545

    Remove console.log

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -52,18 +52,13 @@ const App = () => {
         {nameFilter
           ? persons.map(
               person =>
-                console.log(
-                  "filter",
-                  person,
-                  person.name.includes(nameFilter)
-                ) ||
-                (person.name
+                person.name
                   .toLowerCase()
                   .includes(nameFilter.toLowerCase()) && (
                   <li key={person.name}>
                     {person.name} {person.number}
                   </li>
-                ))
+                )
             )
           : persons.map(person => (
               <li key={person.name}>
