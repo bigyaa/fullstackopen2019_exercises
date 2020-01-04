a2aab7f457e1a5b96a244a8445c98a2eb3c48a10
bigya <bigyabajracharya@gmail.com>
2020-01-04 18:40:25 +0545

    2.16 separate services module

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -1,65 +1,66 @@
 import React, { useState, useEffect } from "react";
+
 import PersonForm from "./PersonForm";
 import Persons from "./Persons";
 import Filter from "./Filter";
-import axios from "axios";
+import personServices from "../services/personServices";
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
-  const [nameFilter, setNameFilter] = useState("");
+	const [persons, setPersons] = useState([]);
+	const [newName, setNewName] = useState("");
+	const [newNumber, setNewNumber] = useState("");
+	const [nameFilter, setNameFilter] = useState("");
 
-  const baseUrl = "http://localhost:3009/persons";
+	const handleNameChange = event => setNewName(event.target.value);
+	const handleNumberChange = event => setNewNumber(event.target.value);
+	const handleNameFilterChange = event => setNameFilter(event.target.value);
 
-  useEffect(() => {
-    axios.get(baseUrl).then(response => setPersons(response.data));
-  }, []);
+	const handleSubmit = event => {
+		event.preventDefault();
 
-  const handleNameChange = event => setNewName(event.target.value);
-  const handleNumberChange = event => setNewNumber(event.target.value);
-  const handleNameFilterChange = event => setNameFilter(event.target.value);
+		if (persons.find(person => person.name === newName)) {
+			alert(`${newName} already exists in the phonebook`);
+		} else {
+			const newPerson = {
+				id: persons.length + 1,
+				name: newName,
+				number: newNumber
+			};
 
-  const handleSubmit = event => {
-    event.preventDefault();
+			personServices
+				.addPerson(newPerson)
+				.then(response => setPersons(persons.concat(response)));
 
-    if (persons.find(person => person.name === newName)) {
-      alert(`${newName} already exists in the phonebook`);
-    } else {
-      axios
-        .post(baseUrl, {
-          id: persons.length + 1,
-          name: newName,
-          number: newNumber
-        })
-        .then(response => setPersons(persons.concat(response.data)));
+			setNewName("");
+			setNewNumber("");
+		}
+	};
 
-      setNewName("");
-      setNewNumber("");
-    }
-  };
+	useEffect(() => {
+		personServices.getAll().then(response => setPersons(response));
+	}, []);
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <Filter
-        nameFilter={nameFilter}
-        handleNameFilterChange={handleNameFilterChange}
-      />
+	return (
+		<div>
+			<h2>Phonebook</h2>
+			<Filter
+				nameFilter={nameFilter}
+				handleNameFilterChange={handleNameFilterChange}
+			/>
 
-      <h2>Add a New Person</h2>
-      <PersonForm
-        newName={newName}
-        newNumber={newNumber}
-        handleNameChange={handleNameChange}
-        handleNumberChange={handleNumberChange}
-        handleSubmit={handleSubmit}
-      />
+			<h2>Add a New Person</h2>
+			<PersonForm
+				newName={newName}
+				newNumber={newNumber}
+				handleNameChange={handleNameChange}
+				handleNumberChange={handleNumberChange}
+				handleSubmit={handleSubmit}
+			/>
 
-      <h2>Numbers</h2>
-      <Persons persons={persons} nameFilter={nameFilter} />
-    </div>
-  );
+			<h2>Numbers</h2>
+			<Persons persons={persons} nameFilter={nameFilter} />
+		</div>
+	);
 };
 
 export default App;
---
+++ part2/phonebook/src/services/personServices.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:3009/persons";
+
+const getAll = () => axios.get(baseUrl).then(response => response.data);
+
+const addPerson = person =>
+  axios.post(baseUrl, person).then(response => response.data);
+
+export default { getAll, addPerson };
