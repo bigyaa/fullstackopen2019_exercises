c3979bec91f65b3cd9184a94d2d3c4631c9d3a3b
bigya <bigya.bajracharya@citytech.global>
2020-01-13 00:28:00 +0545

    part4b done

--- part4/blogList/controllers/blogs.js
+++ part4/blogList/controllers/blogs.js
@@ -3,7 +3,7 @@ const blogsRouter = require("express").Router();
 
 blogsRouter.get("/", async (request, response, next) => {
   const blogs = await Blog.find({});
-  
+
   try {
     response.json(blogs);
   } catch (exception) {
@@ -29,6 +29,27 @@ blogsRouter.delete("/:id", async (request, response, next) => {
   try {
     await Blog.findByIdAndRemove(id);
     response.status(204).end();
+  } catch (exception) {
+    next(exception);
+  }
+});
+
+blogsRouter.put("/:id", async (request, response, next) => {
+  const body = request.body;
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  };
+
+  try {
+  updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true
+  });
+
+    response.json(updatedBlog.toJSON());
   } catch (exception) {
     next(exception);
   }
