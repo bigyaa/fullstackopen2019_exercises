4a4ca7b86436441a5e4fb00b0a5b776ae3bfec41
bigya <bigyabajracharya@gmail.com>
2020-01-26 22:25:06 +0545

    Optimized solution for 6.10

--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -2,42 +2,33 @@ import React from "react";
 import { emptyNotification } from "../reducers/notificationReducer";
 
 const Notification = ({ store }) => {
-	let style;
+	let style = {
+		border: "solid",
+		padding: 10,
+		borderWidth: 1
+	};
 
 	const notification = store.getState().notification;
-
-	if (notification) {
-		style = {
-			border: "solid",
-			padding: 10,
-			borderWidth: 1
-		};
-	} else {
-		style = {
-			display: "none"
-		};
-	}
 
 	let timer;
 
 	const hideNotification = () => {
 		timer = setTimeout(() => {
-			clearTimeout(timer);
 			store.dispatch(emptyNotification());
 		}, 5000);
 	};
-	console.log(timer);
 
 	const showNotification = () => {
 		return <div style={style}>{notification}</div>;
 	};
 
-	return (
+	return notification ? (
 		<div>
+			{clearTimeout(timer)}
 			{showNotification()}
 			{hideNotification()}
 		</div>
-	);
+	) : null;
 };
 
 export default Notification;
